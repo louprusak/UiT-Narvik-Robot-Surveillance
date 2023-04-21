2985e816ba91b273dee3f90ea0c7c1d7119bb8fd
Loup RUSAK <74204559+louprusak@users.noreply.github.com>
2023-04-21 14:36:39 +0200

    Nav Bar and templates pages

--- Include/app.py
+++ Include/app.py
@@ -1,7 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from jinja2 import tests
 
 app = Flask(__name__)
 
+loggedIn = True;
+
 @app.route("/")
-def hello_world():
-    return render_template("home.html", title="Home")
+def login():
+    return render_template("login.html", title="Login")
+
+@app.route("/home")
+def home():
+    if loggedIn:
+        return render_template("home.html", title="Home", loggedIn=loggedIn)
+
+@app.route("/cams")
+def cams():
+    if loggedIn:
+        return render_template("cams.html", title="Cams", loggedIn=loggedIn)
+
+@app.route("/all")
+def all():
+    if loggedIn:
+        return render_template("all.html", title="All Cams", loggedIn=loggedIn)
+
+@app.route("/settings")
+def settings():
+    if loggedIn:
+        return render_template("settings.html", title="Settings", loggedIn=loggedIn)
+
+@app.route("/")
+def log_out():
+    print('yes')
+
