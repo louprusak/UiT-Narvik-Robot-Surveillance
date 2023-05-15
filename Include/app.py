142f3ae161bfa4543960f0bd05aef6efce90fd90
Loup RUSAK <74204559+louprusak@users.noreply.github.com>
2023-05-15 12:00:55 +0200

    Login and Logout ok

--- Include/app.py
+++ Include/app.py
@@ -10,7 +10,7 @@ app.config['SECRET_KEY'] = '92f3fc2bc60b51fa5bd949b418a6ddad'
 # db = SQLAlchemy(app)
 
 #### Static for tests #####
-loggedIn = True;
+loggedIn = False
 username = 'admin'
 password = 'admin'
 admins = {'username': 'admin', 'password': 'admin'}
@@ -18,9 +18,6 @@ cameras = [
     {'name': 'Left view', 'status': 'active', 'src': 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&mute=1&controls=0', 'date': '26/04/2023', 'hour': '14:50:00'},
     {'name': 'Top view', 'status': 'inactive', 'src': 'https://www.youtube.com/embed/Hy8kmNEo1i8?autoplay=1&mute=1&controls=0', 'date': '26/04/2023', 'hour': '14:50:00'},
     {'name': 'Right view', 'status': 'active', 'src': 'https://www.youtube.com/embed/k85mRPqvMbE?autoplay=1&mute=1&controls=0', 'date': '26/04/2023', 'hour': '14:50:00'}
-    # {'name': 'Back view', 'status': 'inactive', 'src': 'static/video.mp4', 'date': '26/04/2023', 'hour': '14:50:00'},
-    # {'name': 'Bottom view', 'status': 'inactive', 'src': 'static/video.mp4', 'date': '26/04/2023', 'hour': '14:50:00'},
-    # {'name': 'Top view', 'status': 'active', 'src': 'static/video.mp4', 'date': '26/04/2023', 'hour': '14:50:00'}
 ]
 
 
@@ -28,11 +25,16 @@ cameras = [
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    global loggedIn
     form = LoginForm()
     if form.validate_on_submit():
         if form.username.data == username and form.password.data == password:
+            print("Login ok")
+            loggedIn = True
             return redirect(url_for('home'))
         else:
+            print("login pas ok")
+            loggedIn = False
             flash('Login unsuccessful. Check username and password', 'danger')
     return render_template("login.html", title="Login", form=form)
 
@@ -45,26 +47,32 @@ def home():
                                loggedIn=loggedIn,
                                activetab='home',
                                cameras = cameras)
-
+    else:
+        return redirect(url_for('login'))
 
 @app.route("/cams")
 def cams():
     if loggedIn:
         return render_template("cams.html", title="Cams", loggedIn=loggedIn, activetab='cams', cameras=cameras)
-
+    else:
+        return redirect(url_for('login'))
 
 @app.route("/all")
 def all():
     if loggedIn:
         return render_template("all.html", title="All Cams", loggedIn=loggedIn, activetab='all', cameras=cameras)
+    else:
+        return redirect(url_for('login'))
+
+# @app.route("/settings")
+# def settings():
+#     if loggedIn:
+#         return render_template("settings.html", title="Settings", loggedIn=loggedIn)
 
 
-@app.route("/settings")
-def settings():
-    if loggedIn:
-        return render_template("settings.html", title="Settings", loggedIn=loggedIn)
-
-
-@app.route("/")
-def log_out():
-    print('yes')
+@app.route("/logout")
+def logout():
+    global loggedIn
+    print('deco')
+    loggedIn = False
+    return redirect(url_for('login'))
