0c7c47320ff457eca29fe087975a8e4e8e84cfff
Loup RUSAK <74204559+louprusak@users.noreply.github.com>
2023-06-29 14:00:18 +0200

    Get video and display ok (without multithreading)

--- Include/app.py
+++ Include/app.py
@@ -5,7 +5,7 @@
 ####----------------------------------------------------####
 ############################################################
 
-from flask import Flask, render_template, flash, redirect, url_for, Response
+from flask import Flask, render_template, flash, redirect, url_for, make_response, Response
 from forms import LoginForm
 import cv2
 import platform
@@ -14,6 +14,9 @@ from urllib.parse import urlparse
 import numpy as np
 import threading
 import zmq
+import time
+import VideoStreamThread
+from queue import Queue
 
 
 ######################################################
@@ -32,9 +35,9 @@ admins = {'username': 'admin', 'password': 'admin'}
 
 # Cameras data
 cam_urls = [
-    'rtsp://192.168.1.30:554/stream1',
-    'rtsp://192.168.1.30:554/stream2',
-    'rtsp://192.168.1.30:554/stream3'
+    'rtsp://10.0.0.30:554/stream1',
+    'rtsp://10.0.0.31:554/stream1',
+    'rtsp://10.0.0.32:554/stream1'
 ]
 cameras = [
     {'name': 'Right View', 'status': '', 'src': 'video_feed_1', 'date': '', 'hour': ''},
@@ -67,6 +70,9 @@ print("Creating sockets...")
 socket1 = context.socket(zmq.SUB)
 socket2 = context.socket(zmq.SUB)
 socket3 = context.socket(zmq.SUB)
+socket1.setsockopt(zmq.RCVTIMEO,0)
+socket2.setsockopt(zmq.RCVTIMEO,0)
+socket3.setsockopt(zmq.RCVTIMEO,0)
 
 # Connect sockets to server
 print("Binding sockets...")
@@ -79,6 +85,28 @@ print("Sockets subscribing...")
 socket1.setsockopt(zmq.SUBSCRIBE, socket_topics[0])
 socket2.setsockopt(zmq.SUBSCRIBE, socket_topics[1])
 socket3.setsockopt(zmq.SUBSCRIBE, socket_topics[2])
+
+last_visualization_time1 = None
+last_visualization_time2 = None
+last_visualization_time3 = None
+
+
+    # # Creation of one thread per camera
+    # print("INIT - Creating threads...")
+    # thread1 = threading.Thread(target=generate_frames)
+    # thread2 = threading.Thread(target=gen_frames_cam2)
+    # thread3 = threading.Thread(target=gen_frames_cam3)
+    #
+    # # Starting all the threads
+    # print("INIT - starting threads...")
+    # thread1.start()
+    # thread2.start()
+    # thread3.start()
+    #
+    # # Waiting all threads to stop
+    # # thread1.join()
+    # # thread2.join()
+    # # thread3.join()
 
 
 ###########################################################
@@ -108,53 +136,123 @@ def initCams():
     for i in range(len(cam_urls)):
         initCam(cameras[i], cam_urls[i])
 
-#### Receive decode and send video to html ####
-def receive_display_video(socket):
-    print("display video : "+str(socket))
+
+def receive_encode_video(socket):
+    global last_visualization_time1
+    frame_time = 0.0001
     while True:
-        # Receiving data from server
-        topic, data = socket.recv_multipart()
-        # data to frames
-        np_data = np.frombuffer(data, np.uint8)
-        decoded_frame = cv2.imdecode(np_data, cv2.IMREAD_COLOR)
-        encoded_frame = cv2.imencode('.jpg',decoded_frame)
-        frame = encoded_frame[1].tobytes()
-        # Send frame to html
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        try:
+            # Receiving data from server
+            topic, data = socket.recv_multipart(zmq.NOBLOCK)
+            # Data to frames
+            np_data = np.frombuffer(data, np.uint8)
+            decoded_frame = cv2.imdecode(np_data, cv2.IMREAD_COLOR)
+            encoded_frame = cv2.imencode('.jpg', decoded_frame)
+            frame = encoded_frame[1].tobytes()
+            # Update when leaving and come back
+            current_time = time.time()
+            if last_visualization_time1 is not None:
+                elapsed_time = current_time - last_visualization_time1
+                skipped_frames = int(elapsed_time / frame_time)
+                for _ in range(skipped_frames):
+                    try:
+                        socket.recv_multipart(zmq.NOBLOCK)
+                    except zmq.error.Again:
+                        break
+            last_visualization_time1 = current_time
+            # Send frame to html
+            yield (
+                b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
+            )
+        except zmq.error.Again:
+            time.sleep(frame_time)
 
-#### Threaded function for generate cam1 frames ####
-def gen_frames_cam1():
-    print("gen frames 1")
-    return receive_display_video(socket1)
-
-#### Threaded function for generate cam2 frames ####
-def gen_frames_cam2():
-    print("gen frames 2")
-    return receive_display_video(socket2)
-
-#### Threaded function for generate cam3 frames ####
-def gen_frames_cam3():
-    print("gen frames 3")
-    return receive_display_video(socket3)
-
-#### Send cam1 video to html ####
 @app.route('/video_feed_1')
 def video_feed_1():
-    # send cam 1 video to html
-    return Response(gen_frames_cam1(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(receive_encode_video(socket1), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-#### Send cam2 video to html ####
+
 @app.route('/video_feed_2')
 def video_feed_2():
-    # send cam 2 video to html
-    return Response(gen_frames_cam2(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    # thread = threading.Thread(target=receive_encode_video, args=(socket2,))
+    # thread.start()
+    return Response(receive_encode_video(socket2), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-#### Send cam3 video to html ####
 @app.route('/video_feed_3')
 def video_feed_3():
-    # send cam =3 video to html
-    return Response(gen_frames_cam3(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    # thread = threading.Thread(target=receive_encode_video, args=(socket3,))
+    # thread.start()
+    return Response(receive_encode_video(socket3), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
+#### Receive decode and send video to html ####
+# def receive_display_video(socket):
+#     print("display video : "+str(socket))
+#     i=1
+#     while True:
+#         print("socket : "+str(socket)+" ,frame : "+str(i))
+#         # Receiving data from server
+#         topic, data = socket.recv_multipart()
+#         # data to frames
+#         np_data = np.frombuffer(data, np.uint8)
+#         decoded_frame = cv2.imdecode(np_data, cv2.IMREAD_COLOR)
+#         encoded_frame = cv2.imencode('.jpg',decoded_frame)
+#         frame = encoded_frame[1].tobytes()
+#         i=i+1
+#         # Send frame to html
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+#         time.sleep(0.01)
+#
+# #### Threaded function for generate cam1 frames ####
+# def gen_frames_cam1():
+#     print("gen frames 1")
+#     return receive_display_video(socket1)
+#
+# #### Threaded function for generate cam2 frames ####
+# def gen_frames_cam2():
+#     print("gen frames 2")
+#     return receive_display_video(socket2)
+#
+# #### Threaded function for generate cam3 frames ####
+# def gen_frames_cam3():
+#     print("gen frames 3")
+#     return receive_display_video(socket3)
+#
+# #### Send cam1 video to html ####
+# @app.route('/video_feed_1')
+# def video_feed_1():
+#     # send cam 1 video to html
+#     response = make_response(gen_frames_cam1())
+#     response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+#     response.headers['Pragma'] = 'no-cache'
+#     response.headers['Expires'] = '0'
+#     response.mimetype = 'multipart/x-mixed-replace; boundary=frame'
+#     return response
+#
+# #### Send cam2 video to html ####
+# @app.route('/video_feed_2')
+# def video_feed_2():
+#     # send cam 2 video to html
+#     response = make_response(gen_frames_cam2())
+#     response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+#     response.headers['Pragma'] = 'no-cache'
+#     response.headers['Expires'] = '0'
+#     response.mimetype = 'multipart/x-mixed-replace; boundary=frame'
+#     return response
+#
+# #### Send cam3 video to html ####
+# @app.route('/video_feed_3')
+# def video_feed_3():
+#     # send cam =3 video to html
+#     response = make_response(gen_frames_cam3())
+#     response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+#     response.headers['Pragma'] = 'no-cache'
+#     response.headers['Expires'] = '0'
+#     response.mimetype = 'multipart/x-mixed-replace; boundary=frame'
+#     return response
 
 
 #####################################################################
@@ -192,6 +290,7 @@ def home():
 @app.route("/cams")
 def cams():
     if is_logged_in:
+
         return render_template("cams.html",
                                title="Cams",
                                activetab='cams',
@@ -222,19 +321,5 @@ def logout():
 #### ---------- Multithreading Configuration ---------- ####
 ############################################################
 
-# Creation of one thread per camera
-print("Creating threads")
-thread1 = threading.Thread(target=gen_frames_cam1)
-thread2 = threading.Thread(target=gen_frames_cam2)
-thread3 = threading.Thread(target=gen_frames_cam3)
 
-# Starting all the threads
-print("starting threads")
-thread1.start()
-thread2.start()
-thread3.start()
 
-# Waiting all threads to stop
-thread1.join()
-thread2.join()
-thread3.join()
