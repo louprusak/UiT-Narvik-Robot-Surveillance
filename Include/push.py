1a7e2672376eb77c70799abf14038157b88253d5
Loup RUSAK <74204559+louprusak@users.noreply.github.com>
2023-07-17 15:52:02 +0200

    Push and pull tests

---
+++ Include/push.py
@@ -0,0 +1,15 @@
+import zmq
+import random
+
+context = zmq.Context()
+
+# Socket with direct access to the sink: used to syncronize start of batch
+sink = context.socket(zmq.PUSH)
+sink.connect("tcp://10.0.0.103:5558")
+
+# Initialize random number generator
+random.seed()
+
+while True:
+    workload = random.randint(1, 100)
+    sink.send(str(workload))
